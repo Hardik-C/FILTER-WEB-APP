ebfb9650b0844d2aee9090509512bafbac2a1b2d
Hardik-C <86589339+Hardik-C@users.noreply.github.com>
2022-09-26 18:15:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ function setup()
 {
     canvas = createCanvas(350,350);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
 function draw()
 {}
 function take_snapshot()
